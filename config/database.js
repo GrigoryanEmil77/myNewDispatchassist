e9c3edc6b7e2453e4bb040a91875596296198776
Emil <egrigoryan264@gmail.com>
2025-03-18 23:53:11 +0400

    change database.js

--- config/database.js
+++ config/database.js
@@ -1,36 +1,10 @@
-// import mongoose from "mongoose";
-
-// const uri = process.env.MONGODB_URI;
-
-// const connectDB = async () => {
-//   try {
-//     if (!uri) {
-//       throw new Error("MONGODB_URI is not defined in environment variables");
-//     }
-
-//     await mongoose.connect(uri); 
-//     // console.log("MongoDB URI:", uri);
-
-//     console.log("Connected successfully to MongoDB");
-//   } catch (error) {
-//     console.error("Error connecting to MongoDB:", error.message);
-//   }
-// };
-
-// export default connectDB;
-
-
-
-
 const mongoose = require("mongoose");
 
 const MONGO_URI = process.env.MONGODB_URI; 
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+    await mongoose.connect(MONGO_URI,  {
       serverSelectionTimeoutMS: 5000, 
     });
     console.log("Connected to MongoDB");
@@ -38,7 +12,30 @@ const connectDB = async () => {
     console.error("Error connecting to MongoDB:", error.message);
     process.exit(1);
   }
-}
+};
 
-export default connectDB;
+module.exports = connectDB; 
+
+
+
+
+// const mongoose = require("mongoose");
+
+// const MONGO_URI = process.env.MONGODB_URI; 
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(MONGO_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       serverSelectionTimeoutMS: 5000, 
+//     });
+//     console.log("Connected to MongoDB");
+//   } catch (error) {
+//     console.error("Error connecting to MongoDB:", error.message);
+//     process.exit(1);
+//   }
+// }
+
+// export default connectDB;
 
