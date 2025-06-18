891f20316906cb65f4876850c56110abe13e88f5
Emil <egrigoryan264@gmail.com>
2025-06-18 22:50:41 +0400

    add new file

---
+++ app/head.js
@@ -0,0 +1,15 @@
+export default function Head() {
+    return (
+      <>
+        <title>Empower Your Logistic Business with Dispatch Assist</title>
+        <meta
+          name="description"
+          content="Dispatch Assist is a platform for truck drivers and companies! With our 24/7 service, we are clearly committed to supporting owner-operators and businesses of all sizes..."
+        />
+        <link rel="icon" href="/favicon.ico?v=4" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png?v=4" />
+        <link rel="canonical" href="https://dispatch-assist.com/" />
+      </>
+    );
+  }
+  
